ae38c09bc48f1260b0b97698a7f266404ada7e00
Conor ladrigan <conorladrigan95@gmail.com>
2022-06-25 15:23:41 +0100

    Work on login route and me route.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,8 @@ import { Request, Response, Router } from 'express';
 import { User } from '../entity/User';
 import { validate, isEmpty } from 'class-validator';
 import bcrypt from 'bcrypt';
-import { PlainObjectToNewEntityTransformer } from 'typeorm/query-builder/transformer/PlainObjectToNewEntityTransformer';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 const register = async (req: Request, res: Response) => {
   console.log(req.body);
@@ -46,13 +47,13 @@ const login = async (req: Request, res: Response) => {
 
   try {
     const errors: { [key: string]: string } = {};
-    const user = await User.findOne({ username });
     if (isEmpty(username)) errors.username = 'Username must not be empty';
     if (isEmpty(password)) errors.password = 'Password must not be empty';
 
     if (Object.keys(errors).length > 0) {
       return res.status(400).json(errors);
     }
+    const user = await User.findOne({ username });
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
@@ -61,12 +62,45 @@ const login = async (req: Request, res: Response) => {
     if (!passwordMatch) {
       return res.status(401).json({ password: 'Password is invalid' });
     }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 3600,
+        path: '/',
+      })
+    );
+    return res.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const me = async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) throw new Error('Unauthenticated');
+
+    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ username });
+
+    if (!user) throw new Error('Unauthenticated');
     return res.json(user);
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: error.message });
+  }
 };
 
 const router = Router();
 router.post('/register', register);
 router.post('/login', login);
+router.post('/me', me);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,25 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cookieparser from 'cookie-parser';
 
 import authRoutes from './routes/auth';
 import trim from './Middleware/trim';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
+app.use(cookieparser());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello');
 });
 app.use('/api/auth', authRoutes);
 
-app.listen(5001, async () => {
+app.listen(process.env.PORT, async () => {
   console.log(`Server running at http://localhost:5000`);
   try {
     await createConnection();
