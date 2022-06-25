ccec6a182e8b783f3fbaa88365cb963c265cee36
Conor ladrigan <conorladrigan95@gmail.com>
2022-06-25 15:46:24 +0100

    create Auth middleware for parsing jwt token and validating user. Create logout route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
-import { User } from '../entity/User';
 import { validate, isEmpty } from 'class-validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
+
+import auth from '../Middleware/auth';
+import { User } from '../entity/User';
 
 const register = async (req: Request, res: Response) => {
   console.log(req.body);
@@ -82,25 +84,27 @@ const login = async (req: Request, res: Response) => {
 };
 
 const me = async (req: Request, res: Response) => {
-  try {
-    const token = req.cookies.token;
-    if (!token) throw new Error('Unauthenticated');
+  return res.json(res.locals.user);
+};
 
-    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
-
-    const user = await User.findOne({ username });
-
-    if (!user) throw new Error('Unauthenticated');
-    return res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ error: error.message });
-  }
+const logout = async (req: Request, res: Response) => {
+  res.set(
+    'Set-Cookie',
+    cookie.serialize('token', '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      expires: new Date(0),
+      path: '/',
+    })
+  );
+  return res.status(200).json({ success: true });
 };
 
 const router = Router();
 router.post('/register', register);
 router.post('/login', login);
-router.post('/me', me);
+router.get('/me', auth, me);
+router.get('/logout', auth, logout);
 
 export default router;
