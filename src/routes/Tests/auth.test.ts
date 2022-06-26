dde441962eac630981fbe22bba92e8abbcb62084
Conor ladrigan <conorladrigan95@gmail.com>
2022-06-27 00:23:14 +0100

    continue adding tests for auth routes

--- src/routes/Tests/auth.test.ts
+++ src/routes/Tests/auth.test.ts
@@ -1,9 +1,16 @@
 import request from 'supertest';
+import { getConnection } from 'typeorm';
+
+import User from '../../entity/User';
 import app from '../../server';
 import { createTypeormConnection } from '../../Utils/createTypeormConnection';
 
 beforeAll(async () => {
   await createTypeormConnection();
+});
+
+afterAll(async () => {
+  await getConnection().close();
 });
 
 it('responds with an HTTP 400 with invalid data', async () => {
@@ -16,6 +23,12 @@ it('responds with an HTTP 400 with invalid data', async () => {
 it('creates a user with valid information', async () => {
   const response = await request(app)
     .post('/api/auth/register')
-    .send({ username: 'bob', email: 'bob@mail.com', password: '123456' })
+    .send({ username: 'conor', email: 'conor@mail.com', password: '123456' })
     .expect(200);
+
+  const con = await getConnection();
+  const users = await User.find({ email: 'conor@mail.com' });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual('conor@mail.com');
 });
