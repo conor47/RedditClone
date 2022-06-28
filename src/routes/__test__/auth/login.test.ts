52c224edbbc73e58b81f044976005fd0fb37fc94
Conor ladrigan <conorladrigan95@gmail.com>
2022-06-28 20:06:55 +0100

    Add function for generating post id and slugs

--- src/routes/__test__/auth/login.test.ts
+++ src/routes/__test__/auth/login.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import app from '../../../server';
 
 it('responds with an HTTP 400 with invalid data', async () => {
-  const response = await request(app)
+  await request(app)
     .post('/api/auth/register')
     .send({ username: '', password: '' })
     .expect(400);
@@ -15,7 +15,7 @@ it('succeeds with correct credentials after valid register', async () => {
     .send({ username: 'bob', email: 'bob@mail.com', password: '123456' })
     .expect(200);
 
-  await request(app)
+  const response = await request(app)
     .post('/api/auth/login')
     .send({ username: 'bob', password: '123456' })
     .expect(200);
