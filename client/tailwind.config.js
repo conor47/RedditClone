e5a6740d205a1e74d88ea7ca8b80a82ace1f0d27
Conor ladrigan <conorladrigan95@gmail.com>
2022-07-05 23:03:31 +0100

    install and configure tailwind css

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{tsx,js,ts,jsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
