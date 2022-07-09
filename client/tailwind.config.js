788636f14b55ca2e08f028dd7ca229b39d0bb0db
Conor ladrigan <conorladrigan95@gmail.com>
2022-07-09 23:07:14 +0100

    Building and styling register page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.tsx'],
   content: [
     './src/pages/**/*.{tsx,js,ts,jsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        70: '17.5rem',
+      },
+    },
   },
   plugins: [],
 };
