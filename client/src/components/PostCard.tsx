e370243bc8eec736dee48483bce909f47d827c5c
Conor ladrigan <conorladrigan95@gmail.com>
2022-07-12 22:24:15 +0100

    Extracted posts into seperate component

---
+++ client/src/components/PostCard.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+import gravatar from '../../public/images/defaultGravatar.jpg';
+import { Post } from '../../types';
+
+dayjs.extend(relativeTime);
+
+interface PostCardProps {
+  post: Post;
+}
+
+const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  return (
+    <div key={post.identifier} className="flex mb-4 bg-white rounded">
+      {/* vote section */}
+      <div className="flex items-center justify-center w-10 bg-gray-200 rounded-l">
+        <p>V</p>
+      </div>
+      {/* post data */}
+      <div className="w-full p-2">
+        <div className="flex items-center">
+          <Link href={`/r/${post.subName}`}>
+            <Image
+              src={gravatar}
+              alt="placeholder gravatar"
+              height="24px"
+              width="24px"
+              className="rounded-full cursor-pointer"
+            />
+          </Link>
+          <Link href={`/r/${post.subName}`}>
+            <a className="text-xs font-bold hover:underline">
+              /r/{post.subName}
+            </a>
+          </Link>
+          <p className="text-xs text-gray-500">
+            <span className="mx-1">•</span> Posted by
+            <Link href={`/u/user`}>
+              <a href="" className="mx-1 hover:underline">
+                {post.username}
+              </a>
+            </Link>
+            <Link href={post.url}>
+              <a className="mx-1 hover:underline">
+                {dayjs(post.createdAt).fromNow()}
+              </a>
+            </Link>
+          </p>
+        </div>
+        <Link href={post.url}>
+          <a href="" className="my-1 text-lg font-medium">
+            {post.title}
+          </a>
+        </Link>
+        {post.body && <p className="my-1 text-sm">{post.body}</p>}
+        <div className="flex">
+          <Link href={post.url}>
+            <a>
+              <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+                <i className="mr-1 fas fa-comment-alt "></i>
+                <span className="font-bold">20 comments</span>
+              </div>
+            </a>
+          </Link>
+
+          <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+            <i className="mr-1 fas fa-share"></i>
+            <span className="font-bold">Share</span>
+          </div>
+          <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+            <i className="mr-1 fas fa-bookmark "></i>
+            <span className="font-bold">Save</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- src/entity/Comment.ts
+++ src/entity/Comment.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import {
   BeforeInsert,
   Column,
@@ -38,6 +39,7 @@ export default class Comment extends Entity {
   @ManyToOne(() => Post, (post) => post.comments, { nullable: false })
   post: Post;
 
+  @Exclude()
   @OneToMany(() => Vote, (vote) => vote.comment)
   votes: Vote[];
 
