d698b61e930e458f059afcc6b252d8c36dc83790
Conor ladrigan <conorladrigan95@gmail.com>
2022-07-20 12:11:16 +0100

    continued work on navbar search functionality. Fetching and displaying searched subs

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,15 +2,19 @@ import Link from 'next/link';
 import RedditLogo from '../images/redditLogo.svg';
 
 import { useAuthDispatch, useAuthState } from '../context/Auth';
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Actions, Sub } from '../../types';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
   const [name, setName] = useState('');
   const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+  const router = useRouter();
 
   // function which posts a logout request for the currently logged in user
   const logout = async (): Promise<void> => {
@@ -20,15 +24,33 @@ const Navbar: React.FC = () => {
   };
 
   // function which makes a search request.
-  const searchSubs = async (subName: string) => {
-    setName(subName);
-    try {
-      const { data } = await axios.get<Sub[]>(`/subs/search/${subName}`);
-      setSubs(data);
-    } catch (error) {
-      console.log(error);
-    }
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await axios.get<Sub[]>(`/subs/search/${name}`);
+          setSubs(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 250)
+    );
   };
+
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName('');
+  };
+
+  useEffect(() => {
+    if (name.trim() === '') {
+      setSubs([]);
+      return;
+    } else {
+      searchSubs();
+    }
+  }, [name]);
 
   return (
     <nav className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 px-5 bg-white">
@@ -44,16 +66,38 @@ const Navbar: React.FC = () => {
         </span>
       </div>
       {/* Search input */}
-      <div className="flex items-center mx-auto transition-all bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      <div className="relative flex items-center mx-auto transition-all bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
           value={name}
-          onChange={(e) => searchSubs(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
           type="text"
           placeholder="search"
           className="py-1 pr-3 bg-transparent rounded focus:outline-none w-160"
         ></input>
+        <div className="absolute inset-x-0 bg-white" style={{ top: '100%' }}>
+          {subs?.map((sub) => (
+            <div
+              key={sub.name}
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+              onClick={() => goToSub(sub.name)}
+            >
+              <Image
+                src={sub.imageUrl}
+                alt="sub"
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+                className="rounded-full"
+              />
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+
       {/* Auth buttons */}
       <div className="flex">
         {!loading &&
