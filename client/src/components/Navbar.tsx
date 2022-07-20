c101ab29c72bc04dbf5841374a133e356657da03
Conor ladrigan <conorladrigan95@gmail.com>
2022-07-20 11:48:30 +0100

    Work on navbar. Making search requests when typing in search bar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,18 +2,32 @@ import Link from 'next/link';
 import RedditLogo from '../images/redditLogo.svg';
 
 import { useAuthDispatch, useAuthState } from '../context/Auth';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import axios from 'axios';
-import { Actions } from '../../types';
+import { Actions, Sub } from '../../types';
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState('');
+  const [subs, setSubs] = useState<Sub[]>([]);
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
 
+  // function which posts a logout request for the currently logged in user
   const logout = async (): Promise<void> => {
     await axios.get('/auth/logout');
     dispatch({ type: Actions.logout });
     window.location.reload();
+  };
+
+  // function which makes a search request.
+  const searchSubs = async (subName: string) => {
+    setName(subName);
+    try {
+      const { data } = await axios.get<Sub[]>(`/subs/search/${subName}`);
+      setSubs(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -33,6 +47,8 @@ const Navbar: React.FC = () => {
       <div className="flex items-center mx-auto transition-all bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
+          value={name}
+          onChange={(e) => searchSubs(e.target.value)}
           type="text"
           placeholder="search"
           className="py-1 pr-3 bg-transparent rounded focus:outline-none w-160"
