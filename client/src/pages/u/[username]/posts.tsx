40401d61cb88171ec8c9ab7cdbc608c2f9311e98
Conor ladrigan <conorladrigan95@gmail.com>
2022-08-23 22:05:07 +0100

    fixed build error on posts and comments pages

--- client/src/pages/u/[username]/posts.tsx
+++ client/src/pages/u/[username]/posts.tsx
@@ -24,46 +24,47 @@ const Posts: React.FC = () => {
   return (
     <div className="container flex pt-5">
       <div className="w-160">
-        {data.submissions
-          .filter((sub: any) => sub.type === 'post')
-          .map((sub: any) => {
-            if (sub.type === 'post') {
-              const post: Post = sub;
+        {data &&
+          data.submissions
+            .filter((sub: any) => sub.type === 'post')
+            .map((sub: any) => {
+              if (sub.type === 'post') {
+                const post: Post = sub;
 
-              return <PostCard key={post.identifier} post={post} />;
-            } else {
-              const comment: Comment = sub;
-              return (
-                <div
-                  key={comment.identifier}
-                  className="flex my-4 bg-white rounded"
-                >
-                  <div className="flex-shrink-0 w-10 py-4 text-center bg-gray-200 rounded-l">
-                    <i className="text-gray-500 fas fa-comment-alt fa-xs"></i>
+                return <PostCard key={post.identifier} post={post} />;
+              } else {
+                const comment: Comment = sub;
+                return (
+                  <div
+                    key={comment.identifier}
+                    className="flex my-4 bg-white rounded"
+                  >
+                    <div className="flex-shrink-0 w-10 py-4 text-center bg-gray-200 rounded-l">
+                      <i className="text-gray-500 fas fa-comment-alt fa-xs"></i>
+                    </div>
+                    <div className="w-full p-2">
+                      <p className="mb-2 text-xs text-gray-500">
+                        {comment.username}
+                        <span> commented on </span>
+                        <Link href={comment.post.url}>
+                          <a className="font-semibold hover:underline cursor:pointer">
+                            {comment.post.title}
+                          </a>
+                        </Link>
+                        <span className="mx-1">•</span>
+                        <Link href={`/r/${comment.post.subName}`}>
+                          <a className="text-black hover:underline cursor:pointer">
+                            /r/{comment.post.subName}
+                          </a>
+                        </Link>
+                      </p>
+                      <hr />
+                      <p>{comment.body}</p>
+                    </div>
                   </div>
-                  <div className="w-full p-2">
-                    <p className="mb-2 text-xs text-gray-500">
-                      {comment.username}
-                      <span> commented on </span>
-                      <Link href={comment.post.url}>
-                        <a className="font-semibold hover:underline cursor:pointer">
-                          {comment.post.title}
-                        </a>
-                      </Link>
-                      <span className="mx-1">•</span>
-                      <Link href={`/r/${comment.post.subName}`}>
-                        <a className="text-black hover:underline cursor:pointer">
-                          /r/{comment.post.subName}
-                        </a>
-                      </Link>
-                    </p>
-                    <hr />
-                    <p>{comment.body}</p>
-                  </div>
-                </div>
-              );
-            }
-          })}
+                );
+              }
+            })}
       </div>
     </div>
   );
