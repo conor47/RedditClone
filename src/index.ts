5e67295bae3f9cb645ab21bc8d8b51bbaf200e45
Conor ladrigan <conorladrigan95@gmail.com>
2022-08-24 17:19:00 +0100

    Begun work on implementing real time messaging with web sockets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import app from './server';
 import { createTypeormConnection } from './Utils/createTypeormConnection';
+import { parse } from 'url';
+import ws from 'ws';
 
-app.listen(process.env.PORT, async () => {
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', (socket) => {
+  socket.on('message', (message) => console.log(message));
+});
+
+const server = app.listen(process.env.PORT, async () => {
   console.log(`Server running at http://localhost:5000`);
   try {
     console.log('creating connection');
@@ -10,5 +19,13 @@ app.listen(process.env.PORT, async () => {
     console.log('database connected');
   } catch (error) {
     console.log(error);
+  }
+});
+server.on('upgrade', (request, socket, head) => {
+  const { pathname } = parse(request.url!);
+  if (pathname == '/foo') {
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+      wsServer.emit('connection', socket, request);
+    });
   }
 });
