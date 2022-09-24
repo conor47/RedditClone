bd9af2b065018066053854ed2d0b859205714640
Conor ladrigan <conorladrigan95@gmail.com>
2022-09-24 14:28:34 +0100

    work on api auth unit tests

---
+++ src/routes/__test__/auth/logout.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+
+import app from '../../../server';
+
+it('fails when not logged in', async () => {
+  await request(app).get('/api/auth/logout').send().expect(401);
+});
+
+it('succeeds when logged in. Cookie is expired', async () => {
+  const cookie = await global.signin();
+  console.log('cookie', cookie);
+
+  const res = await request(app)
+    .get('/api/auth/logout')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  let expiredCookie = res.get('Set-Cookie');
+  expect(expiredCookie[0].substring(24, 54)).toEqual(
+    'Thu, 01 Jan 1970 00:00:00 GMT;'
+  );
+});
